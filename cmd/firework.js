443036d0e0afb3e61aca0104febbe1a2396d49ae
mswgen <mswgen02@gmail.com>
2020-03-20 20:38:17 +0900

    Update firework.js

--- cmd/firework.js
+++ cmd/firework.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'firework',
-    alises: ['전체전송', 'firework', '파이어워크', '불꽃놀이'],
+    alises: ['전체전송', 'firework', '파이어워크', '불꽃놀이', '략ㄷ재가'],
     description: '명령어를 입력한 서버의 모든 채널에 입력한 메세지를 전송합니다.(봇 제작자만 가능)',
     run: async function (client, message, args, option) {
         if (message.author.id != option.ownerId) return;

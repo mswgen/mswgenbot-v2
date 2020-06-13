8b36fbe80922ede4a5c72571e195ee4c288ff374
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-13 15:09:48 +0900

    카테고리 수정

--- cmd/koreanbots_check.js
+++ cmd/koreanbots_check.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'koreanbots',
     alises: ['hellothisisverification'],
     description: 'koreanbots 봇 소유자 확인을 위한 명령어에요.',
-    category: 'botinfo',
+    category: 'info',
     usage: '/hellothisisverification',
     run: async function (client, message, args, option) {
         message.channel.send(`${client.users.cache.get('647736678815105037').tag}(${client.users.cache.get('647736678815105037').id})`);

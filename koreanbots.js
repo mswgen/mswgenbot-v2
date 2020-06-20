181c4f8750b85a52b21a2253dbdbdd4e88ae5c90
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-20 11:45:08 +0900

    버그 픽스

--- koreanbots.js
+++ koreanbots.js
@@ -5,14 +5,18 @@ module.exports = {
             axios.post('https://api.koreanbots.dev/bots/servers', {
                 servers: client.guilds.cache.size
             }, {
-                'Content-Type': 'application/json',
-                token: process.env.KOREANBOTS
+                headers: {
+                    'Content-Type': 'application/json',
+                    token: process.env.KOREANBOTS
+                }
             });
         }, 120000);
     },
     getVotes: user => {
         axios.get(`https://api.koreanbots.dev/bots/voted/${user.id}`, {
-            token: process.env.KOREANBOTS
+            headers: {
+                token: process.env.KOREANBOTS
+            }
         }).then(res => {
             return res.data.voted;
         });

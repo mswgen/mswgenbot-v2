cc01ab880947670d009d275c3097e36c61bb4eea
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-20 12:05:54 +0900

    오타수정

--- cmd/help.js
+++ cmd/help.js
@@ -1,5 +1,5 @@
 ﻿const Discord = require('discord.js');
-const fs = require('fs');
+const axios = require('axios').default;
 module.exports = {
     name: 'help',
     alises: ['도움', '도움말', 'help'],

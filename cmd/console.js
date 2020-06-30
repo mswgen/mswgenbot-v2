95d7557f2f5aba9da91579ddb1d724580a1f9df9
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-30 18:10:42 +0900

    console 커멘드 코드블록 언어 수정

--- cmd/console.js
+++ cmd/console.js
@@ -14,7 +14,7 @@ module.exports = {
                 return x.name == 'loadingCirclebar';
             })} Evaling in console`)
             .setColor(0xffff00)
-            .addField('Input', fn.codeBlock(args.slice(1).join(' '), 'sh'))
+            .addField('Input', fn.codeBlock(args.slice(1).join(' '), 'bash'))
             .setFooter(message.author.tag, message.author.avatarURL({
                 dynamic: true,
                 size: 2048,
@@ -26,19 +26,19 @@ module.exports = {
             const exec = child.exec(args.slice(1).join(' '), {}, function (err, stdout, stderr) {
                 if (err) {
                     embed.setColor(0xff0000)
-                        .addField('Error', fn.codeBlock(err, 'js'))
+                        .addField('Error', fn.codeBlock(err, 'bash'))
                         .setTitle('Eval error... (In console)');
                     m.edit(embed);
                 } else {
                     embed.setTitle('Eval result (In console)')
                     embed.setColor(0x00ffff)
                     if (stdout) {
-                        embed.addField('Output (stdout)', fn.codeBlock(fn.checkLength(stdout), 'sh'))
-                        embed.addField('Type (stdout)', fn.codeBlock(fn.checkLength(typeof stdout), 'sh'))
+                        embed.addField('Output (stdout)', fn.codeBlock(fn.checkLength(stdout), 'bash'))
+                        embed.addField('Type (stdout)', fn.codeBlock(fn.checkLength(typeof stdout), 'bash'))
                     }
                     if (stderr) {
-                        embed.addField('Output (stderr)', fn.codeBlock(fn.checkLength(stderr), 'sh'))
-                        embed.addField('Type (stderr)', fn.codeBlock(fn.checkLength(typeof stderr), 'sh'))
+                        embed.addField('Output (stderr)', fn.codeBlock(fn.checkLength(stderr), 'bash'))
+                        embed.addField('Type (stderr)', fn.codeBlock(fn.checkLength(typeof stderr), 'bash'))
                     }
                     m.edit(embed);
                 }
@@ -46,13 +46,13 @@ module.exports = {
             });
             exec.stdout.on('data', function (data) {
                 message.author.send(`stdout: ${data}`, {
-                    code: 'sh',
+                    code: 'bash',
                     split: true
                 });
             });
             exec.stderr.on('data', function (data) {
                 message.author.send(`stderr: ${data}`, {
-                    code: 'sh',
+                    code: 'bash',
                     split: true
                 });
             });
